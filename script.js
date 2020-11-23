cb34130df4d10eefd74fdec4043af232bc3a5bcc
szabinakov <szabinkov@gmail.com>
2020-11-23 17:59:21 +0000

    refactored script.js, added some border styleng to meain and header

--- script.js
+++ script.js
@@ -3,34 +3,26 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highestScore = 0;
-
+const messageDisplayed = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 document.querySelector(".check").addEventListener("click", function () {
   const myGuess = Number(document.querySelector(".myGuess").value);
 
   if (!myGuess) {
-    document.querySelector(".message").textContent =
-      "You have to type in a number!";
+    messageDisplayed("You have to type in a number!");
   } else if (myGuess === secretNumber) {
-    document.querySelector(".message").textContent = "You win!";
+    messageDisplayed("You win!");
     document.querySelector(".myHighestScore").textContent = score;
     document.querySelector(".hiddenNumber").textContent = secretNumber;
     if (score > highestScore) {
       document.querySelector(".myHighestScore").textContent = score;
     }
-  } else if (myGuess > secretNumber) {
-    if (score > 0) {
+  } else if (myGuess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector(".score").textContent = score;
       score--;
-      document.querySelector(".score").textContent = score;
-      document.querySelector(".message").textContent = "Too High!";
-    } else {
-      document.querySelector(".message").textContent = "Game Over!";
-      document.querySelector(".score").textContent = 0;
-    }
-  } else if (myGuess < secretNumber) {
-    score--;
-    if (score > 0) {
-      document.querySelector(".score").textContent = score;
-      document.querySelector(".message").textContent = "Too Low!";
+      messageDisplayed(myGuess > secretNumber ? "Too High!" : "Too Low");
     } else {
       document.querySelector(".message").textContent = "Game Over!";
       document.querySelector(".score").textContent = 0;
@@ -41,8 +33,9 @@ document.querySelector(".check").addEventListener("click", function () {
 document.querySelector(".reset").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
+
   document.querySelector(".hiddenNumber").textContent = "?";
-  document.querySelector(".message").textContent = "Start the Guessing...";
+  messageDisplayed("Start the Guessing...");
   document.querySelector(".myGuess").value = "";
   document.querySelector(".score").textContent = score;
 });
